234d0287f87ae8b3f62d69408ba6e000ebdd2768
Andrew Baluev <damico750@yandex.ru>
2021-05-08 12:06:58 +0300

    change script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
-const swiper = new Swiper(".swiper-container", {
-    // Количество слайдов на экране
-    slidesPerView: 'auto',
-    // Центрирование
-    centeredSlides: true,
-    // Отступ между слайдами
-    spaceBetween: 0,
-    // Бесконечная прокрутка
-    loop: true,
-    // Автопрокрутка
-    autoplay: {
-        delay: 5000,
-    },
-    speed: 700,
-    // Навигация
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+window.onload = () => {
+    const swiper = new Swiper(".swiper-container", {
+        // Количество слайдов на экране
+        slidesPerView: 'auto',
+        // Центрирование
+        centeredSlides: true,
+        // Отступ между слайдами
+        spaceBetween: 0,
+        // Бесконечная прокрутка
+        loop: true,
+        // Автопрокрутка
+        autoplay: {
+            delay: 5000,
+        },
+        speed: 700,
+        // Навигация
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+};
